be54f97dcb975d2e91354266bd54a2e8e474b08c
Ash Lawson <ash.lawson@cloudconsort.com>
2024-05-21 22:16:06 +0100

    Add Program.cs

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,8 @@
+﻿internal class Program
+{
+    internal static void Main(string[] args)
+    {
+        // This is a simple C# program that prints "Hello, World!" to the console.
+        Console.WriteLine("Hello, World!");
+    }
+}
