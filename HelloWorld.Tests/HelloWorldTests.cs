333bc6aa3dd2be4fced1aba347cb0d95cb9d5174
Ash Lawson <ash.lawson@cloudconsort.com>
2024-05-22 12:51:10 +0100

    Replace string ops with path ops, for environment agnostic consistent results.

--- HelloWorld.Tests/HelloWorldTests.cs
+++ HelloWorld.Tests/HelloWorldTests.cs
@@ -6,16 +6,19 @@ namespace HelloWorld.Tests;
 public class HelloWorldTests
 {
     [Fact]
-    public void HelloWorld_Prints_HelloWorldEx()
+    public void HelloWorld_Prints_HelloWorld()
     {
         // Arrange
         var location = Assembly.GetExecutingAssembly().Location;
         var baseDir = location.Remove(
-        location.IndexOf(
-            "\\HelloWorld.Tests\\bin\\Debug\\net8.0\\HelloWorld.Tests.dll", StringComparison.InvariantCultureIgnoreCase));
+            location.IndexOf(
+                $"{Path.DirectorySeparatorChar}HelloWorld.Tests{Path.DirectorySeparatorChar}bin{Path.DirectorySeparatorChar}Debug{Path.DirectorySeparatorChar}net8.0{Path.DirectorySeparatorChar}HelloWorld.Tests.dll",
+                StringComparison.InvariantCultureIgnoreCase));
+
+        var projectPath = Path.Combine(baseDir, "HelloWorld", "HelloWorld.csproj");
         var process = new Process
         {
-            StartInfo = new ProcessStartInfo("dotnet", $"run --project {baseDir}\\HelloWorld\\HelloWorld.csproj")
+            StartInfo = new ProcessStartInfo("dotnet", $"run --project \"{projectPath}\"")
             {
                 RedirectStandardOutput = true,
                 UseShellExecute = false,
@@ -30,6 +33,6 @@ public class HelloWorldTests
         var output = process.StandardOutput.ReadToEnd();
 
         // Assert
-        Assert.Equal("Hello, World!\r\n", output);
+        Assert.Equal("Hello, World!", output.Trim());
     }
 }
